a26757d16c1bea1254de940dfdcf07da36c511f0
Philip Korsholm <philip.korsholm@hotmail.com>
2023-06-14 13:40:30 +0200

    more cleanup

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,3 @@
 export * from "./button";
 export * from "./divider";
-export * from "./email-subscriber";
 export * from "./tag";
--- src/components/products/index.ts
+++ src/components/products/index.ts
@@ -1,3 +1,2 @@
 export * from "./card";
-export * from "./grid";
 export * from "./tags";
